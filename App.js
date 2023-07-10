27591af9feca83d4c8305e2ac93e37265b2a6c27
MohammedElMO <elamranimohammed104@gmail.com>
2023-07-10 14:00:05 +0100

    innstalling yarn depencies

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, AsyncStorage } from 'react-native';
-
-import { uuidv4 } from './src/utils/uuid';
-import { Timer } from './src/features/timer/Timer';
-import { Focus } from './src/features/focus/Focus';
-import { FocusHistory } from './src/features/focus/FocusHistory';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import { uuidv4 } from "./src/utils/uuid";
+import { Timer } from "./src/features/timer/Timer";
+import { Focus } from "./src/features/focus/Focus";
+import { FocusHistory } from "./src/features/focus/FocusHistory";
 
 export default function App() {
   const [focusSubject, setFocusSubject] = useState(null);
@@ -13,14 +13,14 @@ export default function App() {
 
   const saveFocusHistory = async () => {
     try {
-      AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory));
+      AsyncStorage.setItem("focusHistory", JSON.stringify(focusHistory));
     } catch (e) {
       console.log(e);
     }
   };
   const loadFocusHistory = async () => {
     try {
-      const history = await AsyncStorage.getItem('focusHistory');
+      const history = await AsyncStorage.getItem("focusHistory");
 
       if (history && JSON.parse(history).length) {
         setFocusHistory(JSON.parse(history));
@@ -76,5 +76,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  focusContainer: { flex: 1, backgroundColor: '#252250' },
+  focusContainer: { flex: 1, backgroundColor: "#252250" },
 });
